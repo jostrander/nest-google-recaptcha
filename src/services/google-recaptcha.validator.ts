0e2c1b64308db8ba2a818aa8271733794618a9d9
Jesse Ostrander <jesse.ostrander@gmail.com>
2020-07-04 13:01:19 -0400

    URLEncode post body to verify endpoint

--- src/services/google-recaptcha.validator.ts
+++ src/services/google-recaptcha.validator.ts
@@ -1,5 +1,6 @@
 import { HttpService, InternalServerErrorException, Injectable, Inject } from '@nestjs/common';
 import { GoogleRecaptchaValidatorOptions } from '../interfaces/google-recaptcha-validator-options';
+import * as qs from 'querystring'
 
 @Injectable()
 export class GoogleRecaptchaValidator {
@@ -13,7 +14,7 @@ export class GoogleRecaptchaValidator {
     }
 
     validate(response: string): Promise<boolean> {
-        return this.http.post(this.apiUrl, {secret: this.options.secretKey, response}, {headers: this.headers})
+        return this.http.post(this.apiUrl, qs.stringify({secret: this.options.secretKey, response}), {headers: this.headers})
             .toPromise()
             .then(res => res.data.data.success)
             .catch(e => {
